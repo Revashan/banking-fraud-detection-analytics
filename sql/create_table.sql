1ee9db0073eb53ce0f38f39772f98090dd89a009
Revashan <141326969+Revashan@users.noreply.github.com>
2026-02-27 10:03:43 +0800

    Create create_table.sql

---
+++ sql/create_table.sql
@@ -0,0 +1,10 @@
+\*create_table*\
+
+CREATE TABLE credit_card_transactions (
+  Time FLOAT,
+  Amount DECIMAL(18,2),
+  Class INT,
+  V1 FLOAT, V2 FLOAT, V3 FLOAT, V4 FLOAT, V5 FLOAT,
+  V6 FLOAT, V7 FLOAT, V8 FLOAT, V9 FLOAT, V10 FLOAT
+  -- remaining V columns omitted for brevity
+);
